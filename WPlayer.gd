d05dbeeb46e220adfc39eb3e1f3c3b00a7315a72
Szymon M. Sabat <capt_sms@protonmail.com>
2022-05-31 15:28:45 +0200

    EoD commit

--- WPlayer.gd
+++ WPlayer.gd
@@ -1,70 +1,25 @@
 extends Node
 
-enum Pose {
-	HANDS_TOUCH
-}
-const pose_count = 1
-
-enum PosePoint {
-	NOSE = 0,
-	LEFT_EYE_INNER,
-	LEFT_EYE,
-	LEFT_EYE_OUTER,
-	RIGHT_EYE,
-	RIGHT_EYE_OUTER,
-	LEFT_EAR,
-	RIGHT_EAR,
-	MOUTH_LEFT,
-	MOUTH_RIGHT,
-	LEFT_SHOULDER,
-	RIGHT_SHOULDER,
-	LEFT_ELBOW,
-	RIGHT_ELBOW,
-	LEFT_WRIST,
-	RIGHT_WRIST,
-	LEFT_PINKY,
-	RIGHT_PINKY,
-	LEFT_INDEX,
-	RIGHT_INDEX,
-	LEFT_THUMB,
-	RIGHT_THUMB,
-	LEFT_HIP,
-	RIGHT_HIP,
-	LEFT_KNEE,
-	RIGHT_KNEE,
-	LEFT_ANKLE,
-	RIGHT_ANKLE,
-	LEFT_HEEL,
-	RIGHT_HEEL,
-	LEFT_FOOT_INDEX,
-	RIGHT_FOOT_INDEX
-}
-const point_count = PosePoint.RIGHT_FOOT_INDEX + 1
-
-enum ExtraPosePoint {
-	LEFT_HAND,
-	RIGHT_HAND,
-	LEFT_FOOT,
-	RIGHT_FOOT,
-	NECK,
-	TORSO_CENTRE,
-	ASS
-}
-const extra_count = ExtraPosePoint.ASS + 1
+const glob = preload("res://Globals.gd")
 
 var _points:PoolVector2Array
 var _poses = []
+
+func get_point(pid, extra = false):
+	if extra:
+		pid += glob.point_count
+	return _points[int(pid)]
 
 func set_pose(pid, detected):
 	_poses[pid] = detected
 	
 func set_point(pid, point, extra = false):
 	if extra:
-		pid += point_count
+		pid += glob.point_count
 	_points[int(pid)] = point
 
 func _ready():
-	for _p in range(0, point_count + extra_count):
+	for _p in range(0, glob.point_count + glob.extra_count):
 		_points.push_back(Vector2(-1, -1))
-	for _p in range(0, pose_count):
+	for _p in range(0, glob.pose_count):
 		_poses.push_back(false)
--- Wally.gd
+++ Wally.gd
@@ -1,7 +1,48 @@
 extends Node
 
-# game-specific settings:
+const glob = preload("res://Globals.gd")
 
+# SETTINGS, don't change on runtime
+const cams_distance = 22
+const cams_res = Vector2(960, 540)
+const board_res = Vector2(1920, 1080)
+const default_cams = Vector2(-1, -1)
+const cams_max_idx = 3 # ilość kamer * 2 - 1
+const sgbm = true
+const ct_test_time = 10
+const ct_kp_wait_time = 10
+const draw_webcam_background = false
+const draw_keypoints = true
+const draw_pose = true
+var _poses_on = [0]
+func get_poses_on():
+	return _poses_on
+
+# configuration
+var multi = Vector2(board_res.x / cams_res.x, board_res.y / cams_res.y)
+var move = Vector2(0, 0)
+var pre = Vector2(0, 0)
+
+var _show_webcam_background = false
+func set_show_webcam_background(show):
+	if draw_webcam_background:
+		_show_webcam_background = show
+		if show and not $GameBackgroundView.do_snap:
+			snapping()
+		
+var _show_keypoints = false
+func set_show_keypoints(show):
+	if draw_keypoints:
+		_show_keypoints = show
+func should_show_keypoints():
+	return _show_keypoints
+		
+var _show_pose = false
+func set_show_pose(show):
+	if draw_pose:
+		_show_pose = show
+func should_show_pose():
+	return _show_pose
 
 enum Step {
 	NOT_WORKING = 0,
@@ -18,14 +59,6 @@ enum StartStatus {
 	WAITING = 2
 }
 
-# SETTINGS, don't change on runtime
-const cams_max_idx = 3 # ilość kamer * 2 - 1
-const sgbm = true
-const ct_test_time = 10
-var _poses_on = [0]
-func get_poses_on():
-	return _poses_on
-
 # !--- DON'T CHANGE: ---!
 
 signal started
@@ -37,9 +70,6 @@ var _wc:WallyController
 var _wc_inited = false
 var _camL = -1
 var _camR = -1
-var multi = Vector2(1, 1)
-var move = Vector2(0, 0)
-var pre = Vector2(0, 0)
 
 var _1st_tick = true
 
@@ -50,6 +80,9 @@ var _ct_time_elapsed = 0
 var _ct_bad_idxs = []
 var _ct_abort = false
 var _ct_keypoints_exists = false
+var _ct_kp_waiting = 0
+
+const config_fname = "user://config.json"
 
 func set_step(step):
 	if step == Step.NOT_WORKING:
@@ -76,12 +109,20 @@ func set_step(step):
 	_ct_step = step
 	
 	$WebcamTestView.visible = _ct_step <= Step.AWAITING_KEYPOINTS
-	$GameBackgroundView.visible = _ct_step >= Step.AWAITING_KEYPOINTS
+	$WebcamTestView/TimeoutLabel.visible = false
+	$GameBackgroundView.visible = _ct_step >= Step.AWAITING_KEYPOINTS	
 	$GameView.visible = _ct_step == Step.WORKING
 	$ConfigureView.visible = _ct_step == Step.CONFIGING
+	$Background.visible = _ct_step != Step.WORKING
 	
-	if _ct_step >= Step.AWAITING_KEYPOINTS:
-		$GameBackgroundView.update()
+	set_show_webcam_background(_ct_step >= Step.AWAITING_KEYPOINTS)
+	set_show_keypoints(_ct_step == Step.CONFIGING)
+	set_show_pose(_ct_step > Step.AWAITING_KEYPOINTS)
+	
+	$WebcamTestView.update()
+	$GameBackgroundView.update()
+	$ConfigureView.update()
+	$GameView.update()
 
 func start(camL, camR, use_sgbm):
 	print("Starting...")
@@ -123,11 +164,12 @@ func end():
 		print("Already ended.")
 		
 func snapping():
-	if _ct_step >= Step.AWAITING_KEYPOINTS:
-		$GameBackgroundView.array = _wc.snap()
-		$GameBackgroundView.update()
+	if _show_webcam_background:
+		if _ct_step >= Step.AWAITING_KEYPOINTS:
+			$GameBackgroundView.snap_out = _wc.snap()
+		$GameBackgroundView.do_snap = true
 
-func tick(poses = [$WPlayer.Pose.HANDS_TOUCH]):
+func tick(poses = [glob.Pose.HANDS_TOUCH]):
 	if _1st_tick:
 		_wc.startTicking()
 		_1st_tick = false
@@ -174,6 +216,7 @@ func tick(poses = [$WPlayer.Pose.HANDS_TOUCH]):
 		return true
 	else:
 		return true
+		
 
 func _ready():
 	set_step(Step.NOT_WORKING)
@@ -181,13 +224,16 @@ func _ready():
 	_wc = WallyController.new()
 	print("WallyController created.")
 	$GameBackgroundView.connect("snapping", self, "snapping")
+	self.connect("ticked", $GameBackgroundView, "update")
+	self.connect("ticked", $ConfigureView, "update")
 	
-func _bad_pair():
+func _bad_pair(blacklist):
 	print("Bad pair.")
-	if not _ct_bad_idxs.has(_ct_curr_idx):
+	var curr_bad = _ct_curr_idx
+	if not _ct_bad_idxs.has(_ct_curr_idx) and blacklist:
 		_ct_bad_idxs.push_back(_ct_curr_idx)
 	print("Bad pairs: ", _ct_bad_idxs)
-	while (_ct_bad_idxs.has(_ct_curr_idx)):
+	while (_ct_bad_idxs.has(_ct_curr_idx) or (_ct_curr_idx == curr_bad and _ct_bad_idxs.size() + 1 != _ct_pairs.size())):
 		_ct_curr_idx += 1
 		print("next: ", _ct_curr_idx)
 		if _ct_curr_idx >= _ct_pairs.size():
@@ -201,44 +247,47 @@ func _bad_pair():
 		_ct_abort = true
 		emit_signal("ct_aborted")
 
-const config_fname = "user://config.json"
-
-func _testing_process(_delta):
+func _testing_process(delta):
 	if _ct_abort:
 		return
 		
 	if _ct_step == Step.NOT_WORKING:
 		if _ct_pairs.empty():
 			print("Initing testing pairs...")
+			if default_cams.x >= 0 and default_cams.y >= 0 and default_cams.x != default_cams.y:
+				print("Testing pairs forced to single pair.")
+				_ct_pairs.push_back(default_cams)
+			else:
+				var file = File.new()
+				var def = Vector2(-1, -1)
+				if file.file_exists(config_fname):
+					file.open(config_fname, File.READ)
+					var data = parse_json(file.get_as_text())
+					file.close()
+					if typeof(data) == TYPE_DICTIONARY:
+						def.x = data["camL"]
+						def.y = data["camR"]
+				if def.x >= 0 and def.y >= 0 and def.x <= cams_max_idx and def.y <= cams_max_idx and def.x != def.y:
+					_ct_pairs.push_back(def)
+					print("Default pair loaded.")
+				for c0 in range(0, cams_max_idx + 1):
+					for c1 in range(0, cams_max_idx + 1):
+						var vec = Vector2(c0, c1)
+						if c0 != c1 and (_ct_pairs.size() == 0 or (_ct_pairs.size() > 0 and _ct_pairs[0] != vec)):
+							_ct_pairs.push_back(vec)
+				print("Testing pairs inited.")
+			_ct_curr_idx = 0
 			
-			var file = File.new()
-			var def = Vector2(-1, -1)
-			if file.file_exists(config_fname):
-				file.open(config_fname, File.READ)
-				var data = parse_json(file.get_as_text())
-				file.close()
-				if typeof(data) == TYPE_DICTIONARY:
-					def.x = data["camL"]
-					def.y = data["camR"]
-			if def.x >= 0 and def.y >= 0 and def.x <= cams_max_idx and def.y <= cams_max_idx and def.x != def.y:
-				_ct_pairs.push_back(def)
-				print("Default pair loaded.")
-			for c0 in range(0, cams_max_idx + 1):
-				for c1 in range(0, cams_max_idx + 1):
-					var vec = Vector2(c0, c1)
-					if c0 != c1 and (_ct_pairs.size() == 0 or (_ct_pairs.size() > 0 and _ct_pairs[0] != vec)):
-						_ct_pairs.push_back(vec)
-			print("Testing pairs inited.")
-		_ct_curr_idx = 0
 		if not _ct_pairs.empty() and not _ct_bad_idxs.size() == _ct_pairs.size():
 			set_step(Step.TESTING)
 	elif _ct_step == Step.TESTING:
 		print("Testing pair ", _ct_curr_idx, ": ", _ct_pairs[_ct_curr_idx])
+		$WebcamTestView/Label.text = "Testuję kamery... (" + String(_ct_pairs[_ct_curr_idx].x) + ", " + String(_ct_pairs[_ct_curr_idx].y) + ")"
 		_ct_keypoints_exists = false
 		var _ct_testing = start(_ct_pairs[_ct_curr_idx].x, _ct_pairs[_ct_curr_idx].y, sgbm)
 		if not _ct_testing:
 			print("Pair failed to init.")
-			_bad_pair()
+			_bad_pair(true)
 		else:
 			print("Pair inited.")
 			set_step(Step.WAITING)
@@ -248,11 +297,12 @@ func _testing_process(_delta):
 			return
 		elif _ct_start == StartStatus.SUCCEED:
 			print("Pair started.")
+			_ct_kp_waiting = 0
 			set_step(Step.AWAITING_KEYPOINTS)
 		else:
 			print("Pair failed to start.")
 			set_step(Step.TESTING)
-			_bad_pair()
+			_bad_pair(true)
 	elif _ct_step == Step.AWAITING_KEYPOINTS:
 		if _ct_keypoints_exists:
 			print("Pair tested successfully.")
@@ -267,6 +317,17 @@ func _testing_process(_delta):
 			file.close()
 			
 			set_step(Step.CONFIGING)
+		else:
+			_ct_kp_waiting += delta
+			var left = int(ct_kp_wait_time - _ct_kp_waiting)
+			if left < 0:
+				left = 0
+			$WebcamTestView/TimeoutLabel.set_text(String(left))
+			$WebcamTestView/TimeoutLabel.visible = true
+			if left == 0:
+				print("Didn't find keypoints.")
+				set_step(Step.TESTING)
+				_bad_pair(false)
 	else:
 		return
 		
